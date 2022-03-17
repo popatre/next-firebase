26382b3150d01497f7abb8bb3d5759c7d867de10
popatre <76402245+popatre@users.noreply.github.com>
2022-03-17 19:03:09 +0000

    fix .env variable and add google auth setup

--- components/Navbar.js
+++ components/Navbar.js
@@ -25,7 +25,7 @@ export default function Navbar({}) {
                             </Link>
                         </li>
                         <li>
-                            <Link href={`/$username`}>
+                            <Link href={`/${username}`}>
                                 <img src={`user?.photoURL`} />
                             </Link>
                         </li>
--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,41 @@
+import { auth, googleAuthProvider } from "../lib/firebase";
+
 export default function EnterPage() {
+    const user = null;
+    const username = null;
+
     return (
         <main>
-            <h2>Sign up</h2>
+            {user ? (
+                !username ? (
+                    <UsernameForm />
+                ) : (
+                    <SignOutButton />
+                )
+            ) : (
+                <SignInButton />
+            )}
         </main>
     );
 }
+
+function SignInButton() {
+    const signInWithGoogle = async () => {
+        try {
+            await auth.signInWithPopup(googleAuthProvider);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            Sign In with Google <img src={"/google.png"} alt="" />
+        </button>
+    );
+}
+
+function SignOutButton() {
+    return <button onClick={auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {}
