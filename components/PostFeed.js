2ba37a74874f83e23478ffe82070456e1356d3fa
popatre <76402245+popatre@users.noreply.github.com>
2022-03-21 20:32:28 +0000

    add user profile SSR

---
+++ components/PostFeed.js
@@ -0,0 +1,37 @@
+export default function PostFeed({ posts, admin }) {
+    return posts ? (
+        posts.map((post) => (
+            <PostItem post={post} key={post.slug} admin={admin} />
+        ))
+    ) : (
+        <h1>Loading...</h1>
+    );
+}
+
+function PostItem({ post, admin = false }) {
+    const wordCount = post?.content.trim().split(/\s+/g).length;
+    const minutesToRead = (wordCount / 100 + 1).toFixed(0);
+
+    return (
+        <div className="card">
+            <Link href={`/${post.username}`}>
+                <a>
+                    <strong> By @{post.username}</strong>
+                </a>
+            </Link>
+
+            <Link href={`/${post.username}/${post.slug}`}>
+                <h2>
+                    <a> {post.title}</a>
+                </h2>
+            </Link>
+
+            <footer>
+                <span>
+                    {wordCount} words. {minutesToRead} min read
+                    <span>Heart {post.heardCount}</span>
+                </span>
+            </footer>
+        </div>
+    );
+}
