5d42375808a8efb5b5417a68905eab58eef5394b
popatre <76402245+popatre@users.noreply.github.com>
2022-03-25 20:01:41 +0000

    adds post edit form

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,7 +1,92 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import AuthCheck from "../../components/AuthCheck";
+import { auth, serverTimeStamp, firestore } from "../../lib/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
+import toast from "react-hot-toast";
+
 export default function AdminPostEdit() {
     return (
         <main>
-            <h1>Edit post</h1>
+            <AuthCheck>
+                <PostManager />
+            </AuthCheck>
         </main>
+    );
+}
+
+function PostManager() {
+    const [preview, setPreview] = useState(false);
+    const router = useRouter();
+    const { slug } = router.query;
+
+    const postRef = firestore
+        .collection("users")
+        .doc(auth.currentUser.uid)
+        .collection("posts")
+        .doc(slug);
+    const [post] = useDocumentData(postRef);
+
+    return (
+        <main classname="container">
+            {post && (
+                <>
+                    <section>
+                        <h1>{post.title}</h1>
+                        <p>ID: {post.slug}</p>
+                        <PostForm
+                            postRef={postRef}
+                            defaultValues={post}
+                            preview={preview}
+                        />
+                    </section>
+                    <aside></aside>
+                </>
+            )}
+        </main>
+    );
+}
+
+function PostForm({ postRef, defaultValues, preview }) {
+    const { register, handleSubmit, watch, reset } = useForm({
+        defaultValues,
+        mode: "onChange",
+    });
+
+    const updatePost = async ({ content, published }) => {
+        await postRef.update({
+            content,
+            published,
+            updated_at: serverTimeStamp(),
+        });
+        reset({ content, published });
+        toast.success("Post successfully updated");
+    };
+
+    return (
+        <form onSubmit={handleSubmit(updatePost)}>
+            {preview && (
+                <div className="card">
+                    <ReactMarkdown>{watch("content")}</ReactMarkdown>
+                </div>
+            )}
+            <div className={preview ? "hidden" : "controls"}>
+                <textarea name="content" {...register("content")}></textarea>
+                <fieldset>
+                    <input
+                        {...register("published")}
+                        className="checkbox"
+                        name="published"
+                        type="checkbox"
+                    />
+                    <label>Published</label>
+                </fieldset>
+                <button type="submit" className="btn-green">
+                    Save Changes
+                </button>
+            </div>
+        </form>
     );
 }
