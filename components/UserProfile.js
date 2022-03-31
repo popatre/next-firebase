149a6468ceec5b392e367e358d0a12ba7b28b0bb
popatre <76402245+popatre@users.noreply.github.com>
2022-03-31 17:30:28 +0100

    fix signout button showing on every profile page

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,6 +1,9 @@
 import { auth } from "../lib/firebase";
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
 
 export default function UserProfile({ user }) {
+    const { username } = useContext(UserContext);
     return (
         <>
             <div className="box-center">
@@ -10,7 +13,7 @@ export default function UserProfile({ user }) {
                 </p>
                 <h1>{user.displayName}</h1>
                 <div className="row">
-                    {auth.currentUser?.uid && <SignOutButton />}
+                    {username === user.username && <SignOutButton />}
                 </div>
             </div>
         </>
