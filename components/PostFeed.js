63c24a1f38d09c7f4a406c39732870229f67551c
popatre <76402245+popatre@users.noreply.github.com>
2022-03-31 14:48:36 +0100

    basic delete functionality

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -1,4 +1,19 @@
 import Link from "next/link";
+import Modal from "react-modal";
+import { useState, useContext } from "react";
+import { firestore } from "../lib/firebase";
+import { UserContext } from "../lib/context";
+
+const customStyles = {
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+    },
+};
 
 export default function PostFeed({ posts, admin }) {
     return posts ? (
@@ -13,6 +28,37 @@ export default function PostFeed({ posts, admin }) {
 function PostItem({ post, admin = false }) {
     const wordCount = post?.content.trim().split(/\s+/g).length;
     const minutesToRead = (wordCount / 100 + 1).toFixed(0);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const { user } = useContext(UserContext);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        // subtitle.style.color = "#f00";
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    const handleClick = async () => {
+        try {
+            await firestore
+                .collection("users")
+                .doc(user.uid)
+                .collection("posts")
+                .doc(post.slug)
+                .delete();
+            console.log("deleted successfully");
+            closeModal();
+        } catch (error) {
+            console.log("somethings gone wrong");
+        }
+    };
 
     return (
         <div className="card">
@@ -41,8 +87,26 @@ function PostItem({ post, admin = false }) {
                         {" "}
                         ❤️ {post.heartCount || 0} Hearts
                     </span>
+                    {admin && (
+                        <button onClick={openModal} className="btn-red">
+                            Delete
+                        </button>
+                    )}
                 </span>
             </footer>
+            <Modal
+                isOpen={isOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Confirm deletion"
+            >
+                <h2>Are you sure you want to delete this?</h2>
+                <button className="btn-red" onClick={handleClick}>
+                    Confirm
+                </button>
+                <button onClick={closeModal}>Cancel</button>
+            </Modal>
         </div>
     );
 }
